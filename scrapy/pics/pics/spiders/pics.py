1c8fe3b4d21a940ada9282682d3bce255db0840e
yangsy-c <yangsy-c@glodon.com>
2022-11-03 19:29:01 +0800

    scrapy

--- scrapy/pics/pics/spiders/pics.py
+++ scrapy/pics/pics/spiders/pics.py
@@ -1,14 +1,30 @@
 import scrapy
+# https://www.bilibili.com/video/BV1QY411F7Vt?p=2
+from pics.items import PicsItem
+
 
 class PicsSpider(scrapy.Spider):
-    name = "dmoz"
-    allowed_domains = ["dmoz.org"]
+    name = "pics"
+    # allowed_domains = ["dmoz.org"]
     start_urls = [
         "http://www.ctopgirl.com/pic/136.html",
         "http://www.ctopgirl.com/pic/141.html"
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        # filename = response.url.split("/")[-2]
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        print(response.xpath('/html/body/div/div/p/img'))
+
+        item = PicsItem()
+        for sel in response.xpath('/html/body/div/div/p/img'):
+            item['title'] = "ypp"
+            item['link'] = sel.re('//.*.jpg')[0]
+            item['desc'] = "ypp-pics"
+            print("\n<===========================")
+            print (item['title'], item['link'], item['desc'])
+            print("===========================>\n")
+
+            yield item
+
